8ae7e57f5a2ca4490b5624c979b66040f3c735cf
nikobun8 <106723280+nikobun8@users.noreply.github.com>
2023-08-28 17:32:35 +0900

    Update new.py

--- new.py
+++ new.py
@@ -1,1 +1,10 @@
-a
+import streamlit as st
+
+st.button("button") # ボタン
+st.selectbox("selectbox", ("select1", "select2")) # セレクトボックス
+st.multiselect("multiselectbox", ("select1", "select2")) # 複数選択可能なセレクトボックス
+st.radio("radiobutton", ("radio1", "radio2")) # ラジオボタン
+st.text_input("text input") # 文字入力(1行)
+st.text_area("text area") # 文字入力(複数行)
+st.slider("slider", 0, 100, 50) # スライダー
+st.file_uploader("Choose file") # ファイルアップロード
